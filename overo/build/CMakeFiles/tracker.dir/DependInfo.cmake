8abdbe05c01530c4f2efa88a4112806598dc29b8
Tijmen Smit <code@tijmensmit.nl>
2021-06-17 15:26:24 +0200

    track class

---
+++ overo/build/CMakeFiles/tracker.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tijmens/stack/Embedded Systems/ESL/vision-pan-tilt/overo/tracker.cpp" "CMakeFiles/tracker.dir/tracker.cpp.o" "gcc" "CMakeFiles/tracker.dir/tracker.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
